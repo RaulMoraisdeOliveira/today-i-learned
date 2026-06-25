81ef3c944974ec38eb5eda4cf3bb2d302d801e1c
Raul Morais de Oliveira <raulmorais2010@gmail.com>
2026-06-25 19:55:16 -0300

    fix: adicionar variavel para retorno de abertura de conexao

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseURL = import.meta.env.VITE_SUPABSE_URL;
+const supabaseURL = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-createClient(supabaseURL, supabaseKey);
+export const supabase = createClient(supabaseURL, supabaseKey);
